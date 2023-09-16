0ddaef3731eb50c7a1985a7d2bc6ecc7fc6afb02
Iryna Yatsenko <iryna.didova@gmail.com>
2023-09-16 10:00:48 +0300

    modal done

--- src/components/imageGalleryItem/ImageGalleryItem.js
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -5,13 +5,10 @@ import { Modal } from 'components/modal/Modal';
 export class ImageGalleryItem extends Component {
   state = {
     isModalOpen: false,
-    lightboxInstance: null, // Додайте посилання на інстанс basicLightbox у стан
+    lightboxInstance: null,
   };
 
   openModal = () => {
-    this.setState({ isModalOpen: true });
-
-    // Перевірка, чи створений інстанс basicLightbox
     if (!this.state.lightboxInstance) {
       const { imgLargeUrl, alt } = this.props;
       const lightboxInstance = basicLightbox.create(`
@@ -21,21 +18,20 @@ export class ImageGalleryItem extends Component {
           </div>
         </div>
       `);
-
-      this.setState({ lightboxInstance }, () => {
-        // Показати модальне вікно, коли інстанс готовий
-        this.state.lightboxInstance.show();
-      });
+      lightboxInstance.show();
+      this.setState({ lightboxInstance });
+    } else {
+      this.state.lightboxInstance.show();
     }
+    this.setState({ isModalOpen: true });
   };
 
-  closeModal = () => {
-    if (this.state.lightboxInstance) {
-      // Приховати модальне вікно, якщо інстанс існує
-      this.state.lightboxInstance.close();
-    }
-    this.setState({ isModalOpen: false });
-  };
+  // closeModal = () => {
+  //   if (this.state.lightboxInstance) {
+  //     this.state.lightboxInstance.close();
+  //   }
+  //   this.setState({ isModalOpen: false });
+  // };
 
   render() {
     const { imageUrl, alt, imageLargeUrl } = this.props;
@@ -43,7 +39,7 @@ export class ImageGalleryItem extends Component {
       <li className="ImageGalleryItem" onClick={this.openModal}>
         <img src={imageUrl} alt={alt} />
         {this.state.isModalOpen && (
-          <Modal imgLargeUrl={imageLargeUrl} alt={alt} closeModal={this.closeModal} />
+          <Modal imgLargeUrl={imageLargeUrl} alt={alt} />
         )}
       </li>
     );
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -4,9 +4,13 @@ export const Modal = ({ imgLargeUrl, alt }) => {
   const instance = basicLightbox.create(`
     <div class="Overlay">
       <div className="Modal">
-        <img src=${imgLargeUrl} alt=${alt}/>
+        <img src=${imgLargeUrl} alt=${alt} width="800" height="600"/>
       </div>
     </div>
-  `);
+  `, {
+    onShow: (instance) => {
+      instance.element().querySelector('div').onclick = instance.close
+    }
+  });
   instance.show();
 };
